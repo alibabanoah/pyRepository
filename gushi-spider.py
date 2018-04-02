971aa813e9015c7366d5897b331e1523237ab88c
Alinoah <phone@aliyun.com>
2018-04-02 16:32:31 +0800

    add CommentParse function

--- gushi-spider.py
+++ gushi-spider.py
@@ -3,20 +3,32 @@ from bs4 import BeautifulSoup
 import urllib.parse
 import urllib.request
 import re
-print('开始请求数据......')
 
-tangshiURL = "http://so.gushiwen.org/gushi/tangshi.aspx"
-rongchiURL = "http://so.gushiwen.org/gushi/songsan.aspx"
-chuciURL = "http://so.gushiwen.org/gushi/chuci.aspx"
+ListgushiwenURL = []
+ListshiwendetailURL = []
+
+def GetListgushiwenURL(): # Read URL data from config file 'gushiwenURL.txt' to List Listgushiwen
+    with open("gushiwenURL.txt",encoding='utf-8') as f:
+        for line in f.readlines():
+            ListgushiwenURL.append(line)
+
+GetListgushiwenURL() # Call function GetgushiwenURL to get URL data
+
+def genegushiURL():
+    for listgushiURL in ListgushiwenURL:
+        yield re.split(r'[,]',listgushiURL)
+
+for f in genegushiURL():
+    print(f[0],'output gushiwen')
+
+
 
 def getgushiID():
     gushiID = 787,798,938
     return gushiID
 
-
-def requestgushiwen():
+def requesturl(url,par):
     print('请求数据')
-    url = 'http://so.gushiwen.org/shiwen2017/ajaxshangxi.aspx'
     value= {
          'id':787,
     }
@@ -31,12 +43,29 @@ def getHtml(url,values):
     html = response_result.decode('utf-8')
     return html
 
-def shangxiParser(): #解析赏析
-    shagnxi = requestgushiwen()
-    soup = BeautifulSoup(shagnxi, 'html.parser')
-    asoup = soup.getText()
+def URLParser():
+    for URL in genegushiURL():
+        shagnxi = requesturl(URL[0],par=None)
+        soup = BeautifulSoup(shagnxi, 'html.parser')
+        for link in soup.find_all('a'):
+            if link.get('href')[1:8] == "shiwenv":
+                ListshiwendetailURL.append(link.get('href'))
+URLParser()
 
-    for string in soup.stripped_strings:
-        print(repr(string),'<----')
+def CommentParser(): #解析译文与赏析
+    #for link in ListshiwendetailURL:
+        #alllink = "https://so.gushiwen.org"+link
+        shagnxi = requesturl("https://so.gushiwen.org/shiwenv_85c1b0fcb6f9.aspx",par=None)
+        soup = BeautifulSoup(shagnxi, 'html.parser')
+        s_soup=soup.find_all('div',attrs={"onclick":re.compile(r"shangxiShow.")})
+        f_soup=soup.find_all('div',attrs={"onclick":re.compile(r"fanyiShow.")})
+        print(s_soup,'++++++++++++++++++++')
+        print(f_soup, '++++++++++++++++++++')
+        #for string in soup.stripped_strings:
+        #    print(repr(string),'<----')
+        #print(URL[0],"新的")
 
-print(shangxiParser())
+CommentParser()
+
+#for  link in  ListshiwendetailURL:
+#    print("-----》","https://so.gushiwen.org"+link,"《------")
